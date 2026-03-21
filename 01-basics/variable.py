1fea6dc5168fc8a1ebe09276336b81627a729df7
shilpa16075 <shilpa08500@gmail.com>
2026-03-21 14:30:40 +0530

    first commit

---
+++ 01-basics/variable.py
@@ -0,0 +1,11 @@
+# variable means linking of data to a name. Data is stored in memory, and according to data type, the interpreter reserves the memory space
+# rules: 
+# keyword cannot be used as a variable and other reserved keywords.
+# python is case-sensitive, and hence variable are also case sensitive 
+# A variable cannot start with a number.
+# A Variable is assigned to data by using the assignment operator.
+A = 10
+B = 40
+print(A)
+print(B)
+

43e8ad33425ca3755902033116732a3aadcef47f
shilpa16075 <shilpa08500@gmail.com>
2026-03-21 15:21:14 +0530

    Operators

---
+++ 02-operator/arithemetic operator.py
@@ -0,0 +1,18 @@
+# Arithmetic operator comprise of operands and operators
+# ** Exponent performs eponential calculation on operators
+# / Division 
+# % Modulus
+# + Addition
+# - Subtraction
+# // Floor division : floor division means that after performing the division it returns the lowest integer value as the result
+
+# Let's move to examples
+# Write a program  to print the sum, difference, product.
+A = int(input("Enter a number: "))   # Take numbers for the user
+B = int(input("Enter second number: ")) # The data type of the input numbers is int
+# for sum
+print(A+B)
+# for difference
+print(A-B)
+# for product
+print(A*B)

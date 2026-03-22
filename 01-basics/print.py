3c9ed8033a8027de9172703fae5f5c9fcdc2ceb9
shilpa16075 <shilpa08500@gmail.com>
2026-03-22 11:38:30 +0530

    all about print statement

---
+++ 01-basics/print.py
@@ -0,0 +1,24 @@
+# In python we use print as a function 
+# So Hello World is a string. Anything that is single quotes or double quotes is called string.
+# If you start with single-quotes, then you must end with single quotes. The same holds true for double-quotes.
+print("Hello world")
+# print('python's world')  
+# Here the single quotes is used three times; the single middle quote is a part of the english syntax, not programming syntax. However, the interpreter takes it as a programming syntax.
+# so you can use double quotes here.
+# print('python's world')  
+# Here the single quotes is used three times; the single middle quote is a part of the english syntax, not programming syntax. However, the interpreter takes it as a programming syntax.
+# so you can use double quotes here.
+print ("python's world") 
+# Back Slash /
+# python back slash is to continuation of the string. You can stretch a single statement across multiple lines.
+print("what you think \
+you become")
+# Escape sequence of the string
+# It allow you to put special character such as the tab, the new line, and the backspace into your strings. 
+# \b Backspace
+# \a sound system bell
+#  \n newline
+#  \t horizontal tab 
+#  \\ the \character
+#  \' single quotation mark 
+#  \" double quotation mark
